73adbadd0abedd1f8bd6e1025d688b99e085fdd8
Rose Wills <rmvwills@gmail.com>
2024-01-19 10:46:41 -0500

    init commit

---
+++ spyderweb.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+'''
+SpyderWeb
+
+Version: 0.0.0
+Created: 2024-01-19 (by Rose Wills)
+Status: not finished
+
+This script takes a list of potential home addresses ("homes"), as well
+as a list of destination addresses ("dests"). It returns a csv file with
+the driving distance (dis) and time (eta) between each home (1 row/home,
+1 column/dest for dis, 1 column/dest for eta)
+
+    - home addresses (rows)
+    - driving distance to dest from home (1 column/dest)
+    - driving time to dest from home
+
+'''
