f2a30d75f282b8e5ceed3f2ec8700b76bda0e3c2
Rose Wills <rmvwills@gmail.com>
2024-01-26 10:43:03 -0500

    successfully saves data to csv file.

--- pyderweb.py
+++ pyderweb.py
@@ -17,21 +17,22 @@ with dis in miles, 1 column/dest with eta in minutes)
 
 '''
 
-import csv
+import re
 import random
 import math
 from time import sleep
 import json
+import pandas as pd
 import folium
 
 from geopy.exc import GeocoderTimedOut
-from geopy.geocoders import GoogleV3
 # from geopy.geocoders import Nominatim
+from geopy.geocoders import GoogleV3
 import openrouteservice
 from openrouteservice import convert
 
-geolocator = GoogleV3(api_key="INSERT_GOOGLE_MAPS_API_KEY_HERE")
 # geolocator = Nominatim(user_agent="pyderweb")
+geolocator = GoogleV3(api_key="INSERT_GOOGLE_MAPS_API_KEY_HERE")
 client = openrouteservice.Client(key='INSERT_OPENROUTESERVICE_API_KEY_HERE')
 
 class colors:
@@ -45,9 +46,6 @@ class colors:
     underline = '\033[4m'
     endc = '\033[0m'
 
-
-# testcoors = ((80.21787585263182,6.025423265401452),(80.23990263756545,6.018498276842677))
-# testres = client.directions(testcoors)
 
 homes = { "University of Georgia": "101 Sanford Dr, Athens",
 			"Delaware State University": "1200 N Dupont Hwy, Dover",
@@ -87,46 +85,35 @@ destShort = { "Boyfriend": "698 N Atlantic Ave, Ocean City",
 
 quickAdd = { "St. Mary's College of Maryland": "47645 College Dr, St Marys City" }
 
-
-
 def get_geocode(address, name, attempt=1, maxAttempts=5):
 	try:
 		location = geolocator.geocode(address)
-		# print(name+":",location.address)
 		return location
 		sleep(1)
 
 	except GeocoderTimedOut:
-		# print("GeocoderTimedOut for", address)
 		if attempt <= maxAttempts:
-			# print("timeout for", address+",","re-attempt #"+str(attempt))
 			attempt = attempt + 1
 			get_geocode(address, name, attempt)
 		else:
-			# print("giving up on", name, "("+address+")")
 			errmess = colors.red+"ERROR [get_geocode]: GeocoderTimedOut >>> "+name+"\n\t"+colors.yellow+"giving up after 5 attempts."+colors.endc
 			return errmess
 
 	except AttributeError as e:
-		# print("AttributeError for", address+":", e)
 		errmess = colors.red+"ERROR [get_geocode]: AttributeError >>> "+name+"\n\t"+colors.yellow+str(e)+colors.endc
 		return errmess
 
 	except Exception as e:
-		# print("Error:", name, "failed to run." )
 		errmess = colors.red+"ERROR [get_geocode]: unknown error >>> "+name+"\n\t"+colors.yellow+str(e)+colors.endc
 		return errmess
 	
-		# errmess = colors.red+"get_geocode("+name+") [AttributeError]\n\t"+colors.yellow+str(e)+colors.endc
 
 
-def get_data(startLoc, startName, endLoc, endName):
-
+def get_data(startLoc, endLoc):
 	try:
-		# startCoor = (startLoc.latitude, startLoc.longitude)
-		# endCoor = (endLoc.latitude, endLoc.longitude)
 		startCoorFlip = (startLoc.longitude, startLoc.latitude)
 		endCoorFlip = (endLoc.longitude, endLoc.latitude)
+
 	except Exception as e:
 		errmess = colors.red+"ERROR [get_route]: unknown error >>>\t"+colors.red+str(e)+colors.endc
 		return errmess
@@ -144,23 +131,18 @@ def get_data(startLoc, startName, endLoc, endName):
 	# 	f.write(json.dumps(res,indent=4, sort_keys=True))
 
 
-center = dests["BWI Departures"]
-
-def gen_map(startDict, endDict, focus, mapName):
-	# Get centerpoint of map
-	focusLoc = get_geocode(focus, focus+" (CENTER)")
-	if "Error" in focusLoc:
-		print(colors.red+focusLoc+colors.endc)
-		return
-	else:
-		print("Center Located", "("+focus+")")
+def gen_table(startDict, endDict, csvName, mapDir, genMap=False):
 
 	startLocs = {}
 	endLocs = {}
 	colorDict = {}
 	startLen = 1
 	endLen = 1
-	requestCount = 0
+	# cols = []
+	# for key in endDict.keys():
+	# 	cols.append(re.split(' |\'', key)[0]+"-DIST")
+	# 	cols.append(re.split(' |\'', key)[0]+"-TIME")
+	table = pd.DataFrame(index = startDict.keys())
 
 	for startName in startDict:
 		startLoc = get_geocode(startDict[startName],startName)
@@ -173,7 +155,6 @@ def gen_map(startDict, endDict, focus, mapName):
 			print("...added", "("+locLat+","+locLon+")", startName)
 			if len(startName) > startLen:
 				startLen = len(startName)
-
 
 	for endName in endDict:
 		endLoc = get_geocode(endDict[endName],endName)
@@ -192,13 +173,16 @@ def gen_map(startDict, endDict, focus, mapName):
 		endColor = "rgb("+str(cVals[0])+","+str(cVals[1])+","+str(cVals[2])+")"
 		colorDict[end] = endColor
 	
-	# Generate Maps
+	# Output Data Files
 	for start in startLocs:
-		m = folium.Map(location=(focusLoc.latitude, focusLoc.longitude),zoom_start=7, control_scale=True,tiles="cartodbpositron")
 		print("Generating Route Data for ", start)
+
+		if genMap == True:
+			m = folium.Map(location=(startLocs[start].latitude, startLocs[start].longitude),zoom_start=7, control_scale=True,tiles="cartodbpositron")
+
 		for end in endLocs:
 			sleep(1)
-			dataOut = get_data(endLocs[end], end, startLocs[start], start)
+			dataOut = get_data(endLocs[end], startLocs[start])
 			
 			try:
 				coors, distance, duration = dataOut
@@ -218,62 +202,65 @@ def gen_map(startDict, endDict, focus, mapName):
 			else:
 				humanTime = "(n/a)"
 
-			# Fix Margins on Terminal Output Text
+			# Terminal Output Text
 			endMargin = " " * ( endLen - len(end) )
 			startMargin = " " * ( startLen - len(start) )
 			print(start, startMargin, "<--", end, endMargin, distance, "miles", "("+humanTime+")")
 
-			geometry = client.directions(coors)['routes'][0]['geometry']
-			decoded = convert.decode_polyline(geometry)
-
-			routeName = end+" --> "+start
-
-			labeltxt = "<b><strong>"+routeName+"</strong></b></br>"
-			distancetxt = "<b><strong>"+str(distance)+" miles </strong></b></br>"
-			durationtxt = "<b><strong>"+str(duration)+" mins</strong></b>"
+			# Add to Table
+			table.at[start, re.split(' |\'', end)[0]+" (D)"] = distance
+			table.at[start, re.split(' |\'', end)[0]+" (T)"] = humanTime
 
 
-			routeColor = colorDict[end]
+			if genMap == True:
+				# Generate Map
+				geometry = client.directions(coors)['routes'][0]['geometry']
+				decoded = convert.decode_polyline(geometry)
 
-			fillColor = routeColor
-			color = routeColor
+				routeName = end+" --> "+start
 
-			folium.GeoJson(
-				decoded,
-				name=routeName,
-				style_function=lambda x, fillColor=fillColor, color=color: {
-					"fillColor": fillColor,
-					"color": color,
-				},
-			).add_child(folium.Popup(labeltxt+distancetxt+durationtxt,max_width=300)).add_to(m)
+				labeltxt = "<b><strong>"+routeName+"</strong></b></br>"
+				distancetxt = "<b><strong>"+str(distance)+" miles </strong></b></br>"
+				durationtxt = "<b><strong>"+str(humanTime)+"</strong></b>"
 
-			folium.Marker(
-				location=list(coors[0][::-1]),
-				popup=end,
-				icon=folium.Icon(color="green"),
-			).add_to(m)
+				routeColor = colorDict[end]
+				fillColor = routeColor
+				color = routeColor
 
-			folium.Marker(
-				location=list(coors[1][::-1]),
-				popup=start,
-				icon=folium.Icon(color="red"),
-			).add_to(m)
-			
-		startName = start.replace(" ", "-")
-		startName = startName.replace(".", "")
-		startName = startName.replace(",", "")
-		startName = startName.replace("'", "")
-		saveName = mapName+startName+".html"
-		m.save(saveName)
+				folium.GeoJson(
+					decoded,
+					name=routeName,
+					style_function=lambda x, fillColor=fillColor, color=color: {
+						"fillColor": fillColor,
+						"color": color,
+					},
+				).add_child(folium.Popup(labeltxt+distancetxt+durationtxt,max_width=300)).add_to(m)
 
-gen_map(quickAdd, dests, center, "saved-maps/")
+				folium.Marker(
+					location=list(coors[0][::-1]),
+					popup=end,
+					icon=folium.Icon(color="green"),
+				).add_to(m)
+
+				folium.Marker(
+					location=list(coors[1][::-1]),
+					popup=start,
+					icon=folium.Icon(color="red"),
+				).add_to(m)
+		
+		if genMap == True:
+			mapName = start.replace(" ", "-")
+			mapName = mapName.replace(".", "")
+			mapName = mapName.replace(",", "")
+			mapName = mapName.replace("'", "")
+			mapFile = mapDir+mapName+".html"
+			m.save(mapFile)
+	print(table)
+	table.to_csv(csvName+".csv")
+
+gen_table(homeShort, destShort, "test", "saved-maps/")
 
 
 
-			# Get Data
-			# if requestCount < 20:
-			# 	requestCount += 1
-			# else:
-			# 	print("sleeping...")
-			# 	sleep(5)
-			# 	requestCount = 1
+# testcoors = ((80.21787585263182,6.025423265401452),(80.23990263756545,6.018498276842677))
+# testres = client.directions(testcoors)
